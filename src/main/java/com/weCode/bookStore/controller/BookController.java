36c1373aaa7347ba0fa54a8d4ba23623abb18df8
Thiago Lanza <thlanza@hotmail.com>
2021-06-15 19:36:49 -0300

    first

---
+++ src/main/java/com/weCode/bookStore/controller/BookController.java
@@ -0,0 +1,29 @@
+package com.weCode.bookStore.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.weCode.bookStore.dto.BookDto;
+
+@RestController
+@RequestMapping("api/v1/books")
+public class BookController {
+
+	@GetMapping
+	public ResponseEntity<List<BookDto>> getBooks() {
+		
+		BookDto book = BookDto.builder()
+				.title("My First Book title")
+				.build();
+		
+		List<BookDto> books = new ArrayList<>();
+		books.add(book);
+		return ResponseEntity.ok(books);
+		
+	}
+}

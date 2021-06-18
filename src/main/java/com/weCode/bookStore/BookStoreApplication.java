b2a345e70ba46002bb20e89f0bda15b31e031e0d
Thiago Lanza <thlanza@hotmail.com>
2021-06-18 20:58:07 -0300

    changing security in application.properties

--- src/main/java/com/weCode/bookStore/BookStoreApplication.java
+++ src/main/java/com/weCode/bookStore/BookStoreApplication.java
@@ -1,10 +1,8 @@
 package com.weCode.bookStore;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@EnableAutoConfiguration(exclude = { SecurityAutoConfiguration.class })
+
 public class BookStoreApplication {
 
 	public static void main(String[] args) {

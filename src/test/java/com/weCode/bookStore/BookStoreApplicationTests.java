f6a714ca2ee9824bb0cbeea72af829e3dc745a2f
Thiago Lanza <thlanza@hotmail.com>
2021-06-18 21:03:04 -0300

    succeeed tests

--- src/test/java/com/weCode/bookStore/BookStoreApplicationTests.java
+++ src/test/java/com/weCode/bookStore/BookStoreApplicationTests.java
@@ -1,8 +1,10 @@
 package com.weCode.bookStore;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@AutoConfigureMockMvc(addFilters = false)
 @SpringBootTest
 class BookStoreApplicationTests {
 

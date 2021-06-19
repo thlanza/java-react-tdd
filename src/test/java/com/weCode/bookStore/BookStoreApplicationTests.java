c150bb20551fc9baad125a90da02c46770cbd173
Thiago Lanza <thlanza@hotmail.com>
2021-06-18 21:22:12 -0300

    simetria 2

--- src/test/java/com/weCode/bookStore/BookStoreApplicationTests.java
+++ src/test/java/com/weCode/bookStore/BookStoreApplicationTests.java
@@ -1,11 +1,9 @@
 package com.weCode.bookStore;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@EnableAutoConfiguration(exclude = { SecurityAutoConfiguration.class })
+
 @SpringBootTest
 class BookStoreApplicationTests {
 
